619e44d71424469afbde3f65abdb9887b1bec641
cnlucke <cristy.lucke@gmail.com>
2018-01-15 20:21:27 -0500

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,6 @@
+-- Update the species of the last character in the database to "Martian" by writing an update statement in update.sql.
+
+UPDATE characters SET species = "Martian"
+  WHERE id=(
+    SELECT max(id) FROM characters
+  );
